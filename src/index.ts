0b44c2c85232ab53e91db5f6cc4ee1cb234a9a59
Mitch.Prewitt <mitch.prewitt@asurion.com>
2021-03-07 17:22:57 -0500

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import fs from "fs";
+import path from "path";
+import { Triangle, Vector3} from 'three';
+
+type Vertex = {
+    x: number;
+    y: number;
+    z: number;
+}
+
+const calculate3dArea = (v1: Vertex, v2: Vertex, v3: Vertex): number => {
+    console.log({ v1, v2, v3});
+    const vector1 = new Vector3(v1.x, v1.y, v1.z);
+    const vector2 = new Vector3(v2.x, v2.y, v2.z);
+    const vector3 = new Vector3(v3.x, v3.y, v3.z);
+    const triangle = new Triangle(vector1, vector2, vector3);
+    const area = triangle.getArea();
+    console.log({area});
+
+    return area;
+
+}
+
+const isFacet = (line: Array<string>): boolean => {
+    if (line.includes("facet")) {
+        return true;
+    }
+}
+
+const hasVertex = (line: Array<string>): boolean => {
+    if (line.includes("vertex")) {
+        return true;
+    }
+}
+
+async function processLineByLine(file) {
+    const fileStream = fs.readFileSync(file, "utf-8");
+
+    let count: number = 0;
+    let totalArea: number = 0;
+    let vertexArray: Array<Vertex> = [];
+
+    fileStream.split(/\r?\n/).forEach(line => {
+        const words = line.split(" ");
+        if (isFacet(words)) count += 1;
+        if (hasVertex(words)) {
+            const x = parseFloat(words[words.length -1]);
+            const y = parseFloat(words[words.length -2]);
+            const z = parseFloat(words[words.length -3]);
+            vertexArray.push({x, y, z});
+            if (vertexArray.length === 3) {
+                const area = calculate3dArea(vertexArray[0], vertexArray[1], vertexArray[2]);
+                totalArea += area;
+                vertexArray = [];
+            }
+        }
+    });
+
+    return {count, totalArea};
+  };
+
+  const main = async () => {
+    const stlTestData = await processLineByLine(path.resolve(__dirname, "./assets/Test.stl"));
+    const stlMoonData = await processLineByLine(path.resolve(__dirname, "./assets/Moon.stl"));
+
+    console.log({ stlTestData, stlMoonData });
+  }
+
+  main();
+  
